1f70ff62950d10bc5b2dd57b60bb6e17378440f9
EC2 Default User <ec2-user@ip-172-31-10-224.us-east-2.compute.internal>
2018-11-25 07:38:21 +0000

    Add postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_083441) do
+ActiveRecord::Schema.define(version: 2018_11_25_070509) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_11_22_083441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "age"
   end
 
 end
